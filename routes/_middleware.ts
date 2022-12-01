9f2c863a0ea56dae9ded5490592c732d3c852279
Lucis <luciannojunior@gmail.com>
2022-12-01 16:35:16 -0300

    Update _middleware.ts

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,7 +1,7 @@
 import { withLive } from "$live/live.ts";
 
 export const handler = withLive({
-  siteId: 239,
-  site: "fashion",
-  domains: [],
+  siteId: 279,
+  site: "deus-patria e familia",
+  domains: ["https://deus-patria e familia-deco.deno.dev"],
 });
